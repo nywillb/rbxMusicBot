2b95b09686d0628c015c43c43788e67a7e783cab
William B <william@barkoffusa.com>
2017-09-17 22:41:19 -0400

    commit intial codebase

---
+++ main.js
@@ -0,0 +1,30 @@
+var Botkit = require('botkit');
+
+var controller = Botkit.slackbot();
+var config = require("./config.js");
+
+var bot = controller.spawn({
+	token: config.slack_token
+});
+
+// slack's api only gives these weird codes instead of usernames :(
+var musicChannel = "C753K88NA"
+
+bot.startRTM(function(err,bot,payload) {
+
+	controller.hears([""],'ambient',function(bot, message) {
+		if(message.channel == musicChannel) {
+			bot.api.reactions.add({
+				timestamp: message.ts,
+				channel: message.channel,
+				name: 'thumbs_up',
+			});
+			bot.api.reactions.add({
+				timestamp: message.ts,
+				channel: message.channel,
+				name: 'thumbs_down',
+			});
+		};
+
+	});
+});
